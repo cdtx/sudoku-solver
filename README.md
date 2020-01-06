b1b2e678335b1f723db9152281e1f1612b82a670
Cyril Dutrieux <cyril.dutrieux@gmail.com>
2020-01-06 14:53:34 +0100

    Filled Cell and Block from input

--- README.md
+++ README.md
@@ -9,7 +9,37 @@ $ make clean all
 
 # To test
 ```
-cat test_xx | ./sudoku
+make test_0x
+```
+and for all tests
+```
+make tests
 ```
 
-# 
+# Work timeline
+I choosed to host the projet on github, so it's easier to track evolutions and validate basics of my git knowledges.
+
+Of course I didn't searched neither the solution neither any clue on Google.
+
+## First idea
+I call **Cell** the minimal container for a value from 1 to 9
+I call **Block** a combination of 9 **Cell**, line or square
+A same **Cell** will be part of 3 **Block** (2 lines and a square)
+
+For each **Cell**, I will keep track of the possible values (candidates), starting at 123456789, and then reduce the list by iterating every **Block**.
+
+### Algo
+For each **Block**
+    Reduce()
+        - List fixed values
+        - Remove fixed values from all **Cell** candidates
+    Fix()
+        - A cell has one candidate -> fix it
+        - A cell is the only one with a candidate across the block -> fix it
+
+I'm aware that this method will block if it becomes necessary to choose "randomly" between 2 scenarios. So I'm not sure it will solve the most difficults sudoku. However, after having played some grids, it does not happen on easy ones.
+So I choose to implement this and see later if improvemensts are necessary.
+
+### Step
+I wrote the classes and the code that parses the input, spreads values into **Cells**, and create **Block**
+Quite scolar job, picking values in an array is always a mess...
--- main.cpp
+++ main.cpp
@@ -3,13 +3,104 @@
 #include <vector>
 #include <algorithm>
 
+#define BLOCK_SIZE 3
+#define SIZE (BLOCK_SIZE*BLOCK_SIZE)
+
 using namespace std;
+
+/* Minimal container for a value from 1 to 9 */
+class Cell {
+public:
+   int value = 0;
+   int candidates[SIZE] = {1};
+   Cell(char &str_value) {
+        this->value = str_value - '0';
+   }
+};
+
+/* Combination of 9 Cell, line or square */
+class Block {
+public:
+    Cell *cells[SIZE];
+    // Useful for debug purposes
+    void display(void) {
+        for(int i=0 ; i<SIZE ; i++) {
+            cout << cells[i]->value;
+        }
+        cout << endl;
+    }
+};
+
+class Grid {
+public:
+    Cell *grid[SIZE*SIZE];
+    Block *blocks[3*SIZE];
+    string input;
+
+    void fill(void) {
+        // Read the whole grid as one line
+        for (int i = 0; i < SIZE; i++) {
+            string line;
+            getline(cin, line);
+            this->input += line;
+        }
+        // For each input value, create a cell
+        for (int i = 0; i < SIZE*SIZE; i++) {
+            grid[i] = new Cell(this->input[i]);
+        }
+        // Create the Block and assign them Cell
+        // lines
+        for (int i=0 ; i<SIZE ; i++) {
+            Block *blk = new Block();
+            blocks[0*SIZE + i] = blk;
+            for (int j=0 ; j<SIZE ; j++) {
+                Cell *cell = this->grid[i*SIZE + j];
+                blk->cells[j] = cell;
+            }
+            // blk->display();
+        }
+        // columns
+        for (int i=0 ; i<SIZE ; i++) {
+            Block *blk = new Block();
+            blocks[1*SIZE + i] = blk;
+            for (int j=0 ; j<SIZE ; j++) {
+                Cell *cell = this->grid[i + j*SIZE];
+                blk->cells[j] = cell;
+            }
+            // blk->display();
+        }
+        // squares
+        for (int i_base=0 ; i_base<BLOCK_SIZE ; i_base++) {
+            for (int i_offset=0 ; i_offset<BLOCK_SIZE ; i_offset++) {
+                int i = i_base * i_offset;
+
+                Block *blk = new Block();
+                blocks[2*SIZE + i] = blk;
+                for (int j=0 ; j<BLOCK_SIZE ; j++) {
+                    for (int k=0 ; k<BLOCK_SIZE ; k++) {
+                        Cell *cell = this->grid[i_base*SIZE*BLOCK_SIZE + i_offset*BLOCK_SIZE + j*SIZE + k];
+                        blk->cells[j*BLOCK_SIZE + k] = cell;
+                    }
+                }
+                // blk->display();
+            }
+        }
+        
+    }
+
+    void display(void) {
+        for (int i=0 ; i<SIZE ; i++) {
+            for (int j=0 ; j<SIZE ; j++) {
+                cout << grid[i*SIZE + j]->value;
+            }
+            cout << endl;
+        }
+    }
+};
 
 int main()
 {
-    for (int i = 0; i < 9; i++) {
-        string line;
-        getline(cin, line);
-        cout << line << endl;
-    }
+    Grid *grid = new Grid();
+    grid->fill();
+    grid->display();
 }
